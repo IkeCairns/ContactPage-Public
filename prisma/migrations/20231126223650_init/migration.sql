d716f0b0f63adeb4720018588266a0a3d5c7950b
ikeCairns <zxh03501@nifty.com>
2023-11-27 07:44:46 +0900

    create tables with prisma

---
+++ prisma/migrations/20231126223650_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Post_email_key" ON "Post"("email");
